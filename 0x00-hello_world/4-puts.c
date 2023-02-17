7d678628bb7cb2e623d5f63f45d5832940d6046f
Emmanuel Joga <emmanueljoga@gmail.com>
2023-02-17 01:58:56 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry that prints a line for c program
+ * return: Always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
