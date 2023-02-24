e986902b68568abd31172cb6fe1963fc30e69781
Emmanuel Joga <emmanueljoga@gmail.com>
2023-02-24 23:30:17 +0100

    ./2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies integer
+ * @a: first integer to multiply
+ * @b: second integer to multiply
+ *
+ * Return: The result of multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
